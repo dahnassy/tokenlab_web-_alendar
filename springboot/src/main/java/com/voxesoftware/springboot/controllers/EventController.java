cbce81283d255cc1b06130a661396fc3be1e806a
Nathanael de Souza Leite <Nathanael.leite@datalinkbpo.com.br>
2024-01-26 16:11:07 -0300

    Event Controller pronto

--- springboot/src/main/java/com/voxesoftware/springboot/controllers/EventController.java
+++ springboot/src/main/java/com/voxesoftware/springboot/controllers/EventController.java
@@ -1,29 +1,31 @@
 package com.voxesoftware.springboot.controllers;
 
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.voxesoftware.springboot.dtos.EventRecordDto;
 import com.voxesoftware.springboot.models.EventModel;
 import com.voxesoftware.springboot.repositories.EventRepository;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 
-@RestController
+@RestController("/Events")
 public class EventController {
 	@Autowired
     EventRepository eventRepository;
-	private EventModel eventModel;
+	//private EventModel eventModel;
 	
 	//Cadastrar Eventos
     @PostMapping("/events")
@@ -36,8 +38,8 @@ public class EventController {
     	//passando propriedades de validação do dto para o model
         BeanUtils.copyProperties(eventRecordDto, eventModel);
         
-        for(var product : events.getBody()) {
-            if (1 != 1/*Objects.equals(product.getNumeroControle(), eventModel.getNumeroControle())*/) {
+        for(var event : events.getBody()) {
+            if (Objects.equals(event.getName(), eventModel.getName())) {
                 return ResponseEntity.status(HttpStatus.CONFLICT).body((EventModel) eventRepository.findAll());
             }
             
@@ -47,10 +49,39 @@ public class EventController {
       
     }
     
-  //pegar todos os produtos
+    //pegar todos os eventos
     @GetMapping("/events")
     @Operation(summary = "Tras todos os eventos")
     public ResponseEntity<List<EventModel>> getAllEvents(){
         return ResponseEntity.status(HttpStatus.OK).body(eventRepository.findAll());
+    }
+    
+
+    //atualizar evento
+    @PutMapping("/events/{id}")
+    @Operation(summary = "Edita evento pelo id")
+    public ResponseEntity<Object> updateEvent(@PathVariable(value="id") UUID id,
+                                                @RequestBody @Valid EventRecordDto eventRecordDto){
+        Optional<EventModel> event0 = eventRepository.findById(id);
+        if(event0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Event not found");
+        }
+        var eventModel = event0.get();
+        BeanUtils.copyProperties(eventRecordDto, eventModel);
+        return ResponseEntity.status(HttpStatus.OK).body(eventRepository.save(eventModel));
+    }
+    
+    //deletar produto
+    @DeleteMapping("/events/{id}")
+    @Operation(summary = "Deleta evento pelo id")
+    public ResponseEntity<Object> deleteEvent(@PathVariable(value="id") UUID id){
+        Optional<EventModel> event0 = eventRepository.findById(id);
+
+        if(event0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Event not found!");
+        }
+
+        eventRepository.delete(event0.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Event deleted successfully");
     }
 }
